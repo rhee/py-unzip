22b1482bbfd2c311903391f16db005a2908c86bd
Sang-Hoon RHEE <shr386@hotmail.com>
2016-05-09 22:19:44 +0900

    .

--- py-unzip.py
+++ py-unzip.py
@@ -12,18 +12,26 @@ import zipfile,os.path
 
 def unzip(source_filename, dest_dir):
     with zipfile.ZipFile(source_filename) as zf:
-        for member in zf.infolist():
-            # Path traversal defense copied from
-            # http://hg.python.org/cpython/file/tip/Lib/http/server.py#l789
-            words = member.filename.split('/')
-            path = dest_dir
-            for word in words[:-1]:
-                drive, word = os.path.splitdrive(word)
-                head, word = os.path.split(word)
-                if word in (os.curdir, os.pardir, ''): continue
-                path = os.path.join(path, word)
-            eprint(member.filename)
-            zf.extract(member, path)
+        zf.debug = 3
+        if False: ### if sys.version_info >= (2, 7, 4):
+            # if python 2.7.4+, safe to use 'extractall'
+            zf.extractall(dest_dir)  
+        else:
+            # iterate to avoid '..' security issue
+            for member in zf.infolist():
+                # Path traversal defense copied from
+                # http://hg.python.org/cpython/file/tip/Lib/http/server.py#l789
+                words = member.filename.split('/')
+                path = dest_dir
+                for word in words[:-1]:
+                    drive, word = os.path.splitdrive(word)
+                    head, word = os.path.split(word)
+                    if word in (os.curdir, os.pardir, ''): continue
+                    path = os.path.join(path, word)
+                if not member.filename.endswith('/'):
+                    eprint(member.filename)
+                zf.extract(member, path)
+
 if __name__ == '__main__':
     import sys
     if len(sys.argv)>2:
